e0acd1ba8ca8be28a75a15eee37388e8f0c1d508
tcalettivc <ancaletti@vassar.edu>
2016-08-30 15:35:56 -0400

    First snapshot

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package hellworld;
+
+public class HelloWorld{
+  
+  public static void main(String[] args){
+    // Prints out a statement saying Hello World!
+    System.out.println("Hello World!");
+  }
+}
+  
